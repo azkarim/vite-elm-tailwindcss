b6679bd9ae3ec5cddd800f11146f2c43db0d0720
Azizul Karim <karimazizul1@gmail.com>
2021-12-09 00:24:42 +0600

    changed structure

--- src/Main.elm
+++ src/Main.elm
@@ -1,49 +1,9 @@
 module Main exposing (main)
 
-import Browser
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
 
 
-main : Program String Model Msg
 main =
-    Browser.element
-        { init = init
-        , update = update
-        , view = view
-        , subscriptions = subscriptions
-        }
-
-
-type alias Model =
-    { message : String
-    }
-
-
-init : String -> ( Model, Cmd Msg )
-init flags =
-    ( { message = "Hi I'm Elm compiled from vite-elm" }, Cmd.none )
-
-
-type Msg
-    = Name String
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Name name ->
-            ( { model | message = name }, Cmd.none )
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "w-screen h-screen bg-gray-400" ]
-        [ text "Hello world"
-        ]
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+    div [ class "w-screen h-screen bg-blue-100" ]
+        [ text "Hello World!" ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{js,elm}'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
